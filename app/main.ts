33f4dc2e87824380d4d81609c11f375a126002c9
Amr Adel <amr.adel@pickspace.com>
2024-07-20 14:16:42 +0300

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -2,8 +2,14 @@ import * as net from "net";
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
     connection.on("data", (data) => {
-        console.log("Received:", JSON.stringify(data.toString()));
-        connection.write("+PONG\r\n");
+        const args = data.toString().split(`\r\n`);
+        const cmd = args[2].toLowerCase();
+        if (cmd === 'ping') {
+            connection.write('+PONG\r\n');
+        } else if (cmd === 'echo') {
+            const txt = args[4];
+            connection.write(`$${txt.length}\r\n${txt}\r\n`);
+        }
     })
   });
 
